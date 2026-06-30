3e586c737edb61c368f5d075012c5a980cac8421
CSGoat0 <elturkabdelrahman@gmail.com>
2026-06-30 08:39:24 +0300

    Create InviteAcceptedEvent

---
+++ TheCharityBLL/Events/Events/InvitationEvents/InviteAcceptedEvent.cs
@@ -0,0 +1,12 @@
+﻿using TheCharityDAL.Entities;
+
+namespace TheCharityBLL.Events.Events.InvitationEvents
+{
+    public class InviteAcceptedEvent
+    {
+        public SharedCampaignInvite Invite { get; set; } = null!;
+        public SharedCampaign Campaign { get; set; } = null!;
+        public Organization InvitedOrganization { get; set; } = null!;
+        public User AcceptedByUser { get; set; } = null!;
+    }
+}
