96e5837aaa9f277b86a5183de8a8663cf5e476a1
Abdelrahman Nabil <163023550+CSGoat0@users.noreply.github.com>
2026-07-08 18:03:55 +0300

    Always enable Swagger in the application

--- TheCharityPL/Program.cs
+++ TheCharityPL/Program.cs
@@ -104,11 +104,8 @@ namespace TheCharityPL
             app.MapHealthChecks("/health");// check if the db connected or not
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             //global exception handling middleware
             app.UseMiddleware<ExceptionMiddleware>();
