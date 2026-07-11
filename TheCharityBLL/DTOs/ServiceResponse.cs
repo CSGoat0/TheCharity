a60d18553bf7385bdc158eec4e40f39d9ceada4f
CSGoat0 <elturkabdelrahman@gmail.com>
2026-07-11 15:26:51 +0300

    Remove the second ServiceResponse

--- TheCharityBLL/DTOs/ServiceResponse.cs
+++ TheCharityBLL/DTOs/ServiceResponse.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TheCharityBLL.DTOs
+﻿namespace TheCharityBLL.DTOs
 {
     public class ServiceResponse<T>
     {
         public bool Success { get; set; }
         public string? Message { get; set; }
         public T? Data { get; set; }
-    }
-    public class ServiceResponse
-    {
-        public bool Success { get; set; }
-        public string? Message { get; set; }
-        
     }
 }
